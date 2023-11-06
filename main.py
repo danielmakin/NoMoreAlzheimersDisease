8a284974aa91f4720fe3f627553d458770454943
danielmakin <62213986+danielmakin@users.noreply.github.com>
2023-11-06 13:21:19 +0000

    Created a testing SVM feature
    SVM has been created and accuracy is ~80% with the classifier taking a large dip classifying adjacent stages of AD.

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 def main():
     SCDandMCI, MCIandAD, SCDandAD = clean_data('CSFplasma.csv')
-    print(SCDandAD)
+    construct_svm(SCDandMCI)
     
 
     
 
 def clean_data(fileName):
+
+    #planning on creating SVMs 
+
+    #using data from different sources?
+    #using the same person for different categories?
+    #create SVM from scratch?
+    
 
 
     df = pd.read_csv(fileName)
@@ -18,7 +30,10 @@ def clean_data(fileName):
 
     #get the relevant biomarkers
     df = df.loc[:, ['RID', 'DX', 'MMSE', 'AGE', 'PTAU', 'TAU', 'ABETA']]
+    
+    df = df.replace("<8", "8")
     df = df.replace(">1700", "1700")
+
     #change to required format
     df = df.replace("CN", "SCD")
     df = df.replace("Dementia", "AD")
@@ -39,4 +54,30 @@ def clean_data(fileName):
 
     return SCDMCI, MCIAD, SCDAD
 
+def construct_svm(dataset):
+    #independant
+    X = dataset.iloc[:, [2, 3, 4, 5, 6]].values
+    #dependant
+    y = dataset.iloc[:, 1].values
+    
+    #split into testing and training sets (0.25 for testing)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.fit_transform(X_test)
+
+    #fit the SVM to the training set
+    classifier = SVC(kernel='rbf', random_state=0)
+    classifier.fit(X_train, y_train)
+
+    #test the classifier
+    y_pred = classifier.predict(X_test)
+
+    #construct a confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    print(accuracy_score(y_test, y_pred))
+
+    
 main()
