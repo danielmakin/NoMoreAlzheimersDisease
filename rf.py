c26878be9497b202ce662278aaf716b3f416fdaf
danielmakin <daniel.makin10@gmail.com>
2024-03-08 22:48:05 +0000

    not Much

--- rf.py
+++ rf.py
@@ -6,11 +6,13 @@ def test(path, fields):
     # Before Removing Outliers
     print("Computing Results.......")
     rf.hyper_parameter_selection(iterations=50)
+    rf.test(metrics=True)
 
 
 
 
 def main():
-    test('PreProcessingFiles/Data/PreProcessedData/File1/UnCleanData/data.csv', ['DX', 'PLASMA_NFL', 'PLASMATAU', 'AB4240', 'MMSE', 'AGE', 'PTEDUCAT'])
+    # test('PreProcessingFiles/Data/PreProcessedData/File1/UnCleanData/data.csv', ['DX', 'PLASMA_NFL', 'PLASMATAU', 'AB4240', 'MMSE', 'AGE', 'PTEDUCAT'])
+    test('PreProcessingFiles/Data/PreProcessedData/File2/UnCleanData/data.csv', ['DX','PLASMA_NFL','PLASMAPTAU181','AB4240', 'TAU','MMSE','PTEDUCAT','AGE','PTGENDER'])
     
 main()
